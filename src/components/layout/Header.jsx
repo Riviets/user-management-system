68d3426a036fb32a9f549ceed6b0f0f3cfa55434
Riviets <vladvladpobuta@gmal.com>
2025-03-10 12:59:21 +0000

    Add Spinner component. Use Spinner in Header component

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,23 +1,28 @@
 import {useState, useEffect} from 'react'
 import {authService} from '../../services/authService'
 import ModalConfirm from './ModalConfirm'
+import Spinner from '../utils/Spinner'
 
 function Header(){
     const [error, setError] = useState(null)
     const [user, setUser] = useState({})
     const [isLogoutModalOpen, setIsLogoutModalOpen] = useState(false)
+    const [isLoading, setIsLoading] = useState(false)
 
     useEffect(()=>{
         async function getCurrentUser(){
             try{
+                setIsLoading(true)
                 const response = await authService.getCurrentUser()
                 setUser(response)
+                setIsLoading(false)
             }
             catch(err){
                 console.error('Error fetching user:', error.response?.data || error.message)
                 setError(err)
             }
         }
+
         getCurrentUser()
     }, [])
 
@@ -49,8 +54,10 @@ function Header(){
             <button className='btn border-red-800 bg-red-500 hover:bg-red-700' 
             onClick={handleLogout}>Log out</button>
                 <div className='flex items-center gap-8'>
-                    <p className='font-semibold text-xl'>{user.firstName} {user.lastName} </p>
-                    <img className='bg-white p-3 rounded-full w-20 border-3 border-gray-600 shadow-xl hidden sm:flex' src={user.image} alt="Profile image" />
+                    <p className='font-semibold text-xl'>{user?.firstName} {user?.lastName} </p>
+                    {isLoading ? 
+                        <div><Spinner /></div> : 
+                        <img className='bg-white p-3 rounded-full w-20 h-20 border-3 border-gray-600 shadow-xl hidden sm:flex' src={user?.image} alt="Profile image" />}
                 </div>
           </div>
           {isLogoutModalOpen && <ModalConfirm isOpened={isLogoutModalOpen} message={'logout'} onConfirm={confirmLogout} onCancel={cancelLogout}/>}
